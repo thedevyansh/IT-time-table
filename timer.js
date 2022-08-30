362487f36d81b08289a04598d9083d13b3e07be2
thedevyansh <schawla333333@gmail.com>
2022-08-30 07:18:48 +0530

    Add day map

--- timer.js
+++ timer.js
@@ -53,6 +53,16 @@ var days = {
   sunday: [],
 };
 
+var dayMap = {
+  0: "sunday",
+  1: "monday",
+  2: "tuesday",
+  3: "wednesday",
+  4: "thursday",
+  5: "friday",
+  6: "saturday"
+}
+
 // ******************************************************************
 
 const show_image_and_text = () => {
